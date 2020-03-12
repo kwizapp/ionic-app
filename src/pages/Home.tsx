6b327d988a47bb6ebb9ae42176b54afd39399c41
Roland Schlaefli <rolandschlaefli@gmail.com>
2020-03-12 22:21:49 +0100

    Add basic home screen with redirect fab

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,28 @@
-import './Home.css'
-
+import React from 'react'
 import {
-  IonButton,
   IonContent,
-  IonHeader,
   IonImg,
   IonPage,
-  IonTitle,
-  IonToolbar,
+  IonIcon,
+  IonFab,
+  IonFabButton,
+  IonText,
 } from '@ionic/react'
-import React from 'react'
+import { playCircleOutline } from 'ionicons/icons'
+
+import './Home.css'
 
 function Home(): React.ReactElement {
   return (
     <IonPage id="home-page">
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>kwiz</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
-        <IonImg src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.1JyjpozrR0uxtS0ddishQwHaFP%26pid%3DApi&f=1" />
-        <IonButton routerLink="/poster">START</IonButton>
+        <IonImg src="assets/kwiz_logo.png" />
+        <IonText>I like to movie movie ...</IonText>
+        <IonFab vertical="bottom" horizontal="center">
+          <IonFabButton color="primary" routerLink="/poster">
+            <IonIcon icon={playCircleOutline} />
+          </IonFabButton>
+        </IonFab>
       </IonContent>
     </IonPage>
   )

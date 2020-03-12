e4004a512e07357467643aa128942bba0fe08663
Roland Schlaefli <rolandschlaefli@gmail.com>
2020-03-12 22:22:01 +0100

    Add basic poster view with countdown and blur

--- src/pages/Poster.tsx
+++ src/pages/Poster.tsx
@@ -1,16 +1,58 @@
-import React from 'react'
-import { IonContent, IonPage, IonImg, IonFooter } from '@ionic/react'
+import React, { useState, useEffect } from 'react'
+import {
+  IonContent,
+  IonPage,
+  IonText,
+  IonFooter,
+  IonIcon,
+  IonImg,
+} from '@ionic/react'
+import { heart, flash, heartOutline } from 'ionicons/icons'
 
 import './Poster.css'
 
-function Poster(): React.ReactElement {
+function Poster({ history }: any): React.ReactElement {
+  const [secondsRemaining, setSecondsRemaining] = useState(11)
+
+  useEffect(() => {
+    const trigger = setInterval(() => {
+      setSecondsRemaining(prevSeconds => {
+        if (prevSeconds <= 1) {
+          clearInterval(trigger)
+          history.push('/')
+          return 0
+        }
+        return prevSeconds - 1
+      })
+    }, 1000)
+
+    return () => {
+      clearInterval(trigger)
+    }
+  }, [])
+
   return (
-    <IonPage id="poster">
-      <IonContent fullscreen>
-        <p>Guess the Movie Title</p>
-        <IonImg src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fgamespot1.cbsistatic.com%2Fuploads%2Foriginal%2F1582%2F15828986%2F3606754-witcher%2520poster.jpg&f=1&nofb=1" />
+    <IonPage id="poster" className={`remaining-${secondsRemaining}`}>
+      <IonContent>
+        <IonText>
+          <h1>Guess the movie title</h1>
+        </IonText>
+        <div id="poster-container">
+          <div id="timer">{secondsRemaining}</div>
+          <IonImg src="assets/poster_witcher.png" />
+        </div>
       </IonContent>
-      <IonFooter>POINTS HEARTS</IonFooter>
+      <IonFooter>
+        <div id="points">
+          <IonIcon icon={flash} />
+          12&apos;489
+        </div>
+        <div id="lives">
+          <IonIcon icon={heartOutline} />
+          <IonIcon icon={heart} />
+          <IonIcon icon={heart} />
+        </div>
+      </IonFooter>
     </IonPage>
   )
 }

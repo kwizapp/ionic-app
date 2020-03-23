06ae2d74d68f6f0b70e2fcb3674e49d4b5e377be
Nik Zaugg <zauggnik@gmail.com>
2020-03-23 19:35:12 +0100

    Configure eslint and prettier with @kwizzapp/eslint-dotfiles (#25)
    * add @kwizzapp/eslint-config package
    - add .npmrc
    - also add eslint-plugin-prettier
    
    * update workflow
    - add github registry and GITHUB_TOKEN
    
    * add .npmrc.ci
    
    * replace old eslint config with config from @kwizzapp/eslint-config
    
    * replace old prettier config with config from @kwizzapp/eslint-config
    
    * adjust Dockerfiles for the Github Package Repository
    
    * update workflow
    
    * update workflow
    
    * update README
    
    * use @kwizapp/kwiz-dotfiles
    
    * allow minor versions for kwiz dotfiles
    
    * update README

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,3 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'prettier',
-    'prettier/@typescript-eslint',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-unused-vars': 'error',
-  },
-  settings: {
-    react: {
-      pragma: 'React',
-      version: 'detect',
-    },
-  },
+  extends: ['@kwizapp/eslint-config-ts-react'],
 }

27c6dc666b742fca9141ce9b65296f07f74141d0
Nik Zaugg <zauggnik@gmail.com>
2020-03-30 08:49:19 +0200

    add simple ionic animation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import './Home.css'
 
 import {
+  CreateAnimation,
   IonCol,
   IonContent,
   IonGrid,
@@ -9,13 +10,19 @@ import {
   IonRow,
   IonText,
 } from '@ionic/react'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useHistory } from 'react-router'
 
 function Home(): React.ReactElement {
+  const animationRef = useRef<React.RefObject<CreateAnimation> | any>()
+
   const history = useHistory()
 
   const navigateNext = () => history.push('/poster')
+
+  useEffect(() => {
+    animationRef.current?.animation.play()
+  }, [])
 
   return (
     <IonPage id="home-page">
@@ -35,8 +42,36 @@ function Home(): React.ReactElement {
               <IonRow class="ion-align-items-center">
                 <IonGrid>
                   <IonRow class="ion-align-items-center">
-                    <IonCol size="7" class="ion-text-center">
-                      <IonImg src="assets/kwiz_logo_frame_1.svg" />
+                    <IonCol size="7">
+                      <CreateAnimation
+                        ref={animationRef}
+                        duration={200}
+                        iterations={Infinity}
+                        keyframes={[
+                          {
+                            offset: 0,
+                            background:
+                              'url(assets/kwiz_logo_frame_1.svg) no-repeat',
+                          },
+                          {
+                            offset: 0.25,
+                            background:
+                              'url(assets/kwiz_logo_frame_2.svg) no-repeat',
+                          },
+                          {
+                            offset: 0.5,
+                            background:
+                              'url(assets/kwiz_logo_frame_3.svg) no-repeat',
+                          },
+                          {
+                            offset: 1,
+                            background:
+                              'url(assets/kwiz_logo_frame_4.svg) no-repeat',
+                          },
+                        ]}
+                      >
+                        <div style={{ height: '120px' }}></div>
+                      </CreateAnimation>
                     </IonCol>
                     <IonCol>
                       <IonImg src="assets/app_title.svg" />

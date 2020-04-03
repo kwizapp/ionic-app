8319d5cab4180c62154ea992623c069e5261f5e1
Nik Zaugg <zauggnik@gmail.com>
2020-04-03 08:32:36 +0200

    Connect Poster Screen to API to fetch random Posters (#57)
    * Configure eslint and prettier with @kwizzapp/eslint-dotfiles (#25)
    
    * add @kwizzapp/eslint-config package
    - add .npmrc
    - also add eslint-plugin-prettier
    
    * update workflow
    - add github registry and GITHUB_TOKEN
    
    * add .npmrc.ci
    
    * replace old eslint config with config from @kwizzapp/eslint-config
    
    * replace old prettier config with config from @kwizzapp/eslint-config
    
    * adjust Dockerfiles for the Github Package Repository
    
    * update workflow
    
    * update workflow
    
    * update README
    
    * use @kwizapp/kwiz-dotfiles
    
    * allow minor versions for kwiz dotfiles
    
    * update README
    
    * move routes into AppRouter component
    
    * clear interval correctly after page transition
    
    * formatting
    
    * Add basic navigation and pages for the MVP
    
    * use Ionic lifecycle hooks
    - useIonViewWillEnter
    - useIonViewWillLeave
    
    * reset secondsRemaining on useIonViewWillEnter
    - also add settings.ts for default values
    
    * add custom kwiz logo and keyframes
    
    * add logo to home screen
    
    * update svgs
    
    * add simple ionic animation
    
    * stack items vertically
    
    * remove Home.css
    
    * extract animation into separate component
    
    * add custom favicon
    
    * update animation ref handle
    
    * add @apollo/client
    
    * add animated loading screen
    
    * add ApolloProvider
    
    * add @apollo/client and graphql
    
    * add simple query to fetch a random poster
    
    * Use network-only and define cache key for movie
    
    * Add types to useQuery, fix: temporarily disable member-delimiter-style
    
    * fix for RandomMovieData undefined
    
    * Fix: Refetch on poster screen (#58)
    
    * Fix: Refetch on poster screen
    
    * Lockfile cleanup
    
    * cleanup imports
    
    * add blur animation
    - remove css animation
    
    * start off less blurry
    
    * Poster Page
    - clean up styles a bit
    - remove unneeded css
    
    * add TODO for storing secondsRemaining in a global store
    
    * add every dependency to "dependency"
    - remove "devDependency"
    
    * use new kwiz-dotfiles
    
    * remove console.log statements
    
    Co-authored-by: Roland Schlaefli <rolandschlaefli@gmail.com>

--- src/pages/Poster.tsx
+++ src/pages/Poster.tsx
@@ -1,81 +1,156 @@
 import './Poster.css'
 
+import { gql, useQuery } from '@apollo/client'
 import {
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
   IonContent,
-  IonFooter,
   IonIcon,
   IonImg,
   IonPage,
   IonText,
+  useIonViewDidLeave,
   useIonViewWillEnter,
-  useIonViewWillLeave,
 } from '@ionic/react'
 import { flash, heart, heartOutline } from 'ionicons/icons'
 import React, { useEffect, useRef, useState } from 'react'
 import { useHistory } from 'react-router'
 
+import BlurAnimated from '../components/animations/BlurAnimated'
+import Loading from '../components/loading/Loading'
 import { QUESTION_TIME } from '../settings'
+
+interface Movie {
+  imdbId: string
+  title: string
+  releaseYear: number
+  posterPath: string
+}
+
+interface RandomMovieData {
+  movie: Movie
+}
+
+const RANDOM_MOVIE = gql`
+  {
+    movie {
+      imdbId
+      title
+      releaseYear
+      posterPath
+    }
+  }
+`
 
 function Poster(): React.ReactElement {
   // ref to interval, gets cleared on page leave
-  const countDownRef = useRef<any>()
-
-  const [secondsRemaining, setSecondsRemaining] = useState(11)
-
+  const countDownRef = useRef<any>(null)
   const history = useHistory()
 
-  const navigateNext = () => history.push('/question')
+  const [isPosterExpired, setIsPosterExpired] = useState(false)
+  const [secondsRemaining, setSecondsRemaining] = useState(QUESTION_TIME)
 
-  // [enter] Ionic lifecycle hook
+  const { loading, error, data, refetch } = useQuery<RandomMovieData>(
+    RANDOM_MOVIE,
+  )
+
+  // if the current poster is expired, load a new one
+  useIonViewWillEnter(async () => {
+    // ensure that we only refetch if the poster has expired
+    if (isPosterExpired) {
+      await refetch()
+      setIsPosterExpired(false)
+    }
+  }, [isPosterExpired])
+
+  // setup a countdown for poster expiration
   useIonViewWillEnter(() => {
-    setSecondsRemaining(QUESTION_TIME)
-    countDownRef.current = setInterval(() => {
-      setSecondsRemaining(prevSeconds => {
-        if (prevSeconds <= 1) {
-          return 0
-        }
-        return prevSeconds - 1
-      })
-    }, 1000)
+    // ensure that only one countdown can be set at a time
+    if (countDownRef.current === null) {
+      // reset the remaining time
+      setSecondsRemaining(QUESTION_TIME)
+
+      // start a countdown
+      countDownRef.current = setInterval(() => {
+        setSecondsRemaining(prevSeconds => {
+          if (prevSeconds <= 1) {
+            return 0
+          }
+          return prevSeconds - 1
+        })
+      }, 1000)
+    }
   })
 
-  // [leave] Ionic lifecycle hook
-  useIonViewWillLeave(() => {
+  // clean up the countdown timer when the page is left
+  // and expire the current poster such that we get a new one next time
+  useIonViewDidLeave(() => {
     // clean up interval
     clearInterval(countDownRef.current)
+    countDownRef.current = null
+
+    // set the current poster to be expired
+    setIsPosterExpired(true)
   })
 
+  // create a handler for page navigation
+  const navigateNext = () => history.replace('/question')
+
+  const handleClick = () => {
+    // TODO: add remaining seconds to global store for next screen
+
+    navigateNext()
+  }
+
+  // track the remaining seconds and redirect to the next page on 0
   useEffect(() => {
-    // go to next screen if time is up
     if (secondsRemaining === 0) navigateNext()
   }, [secondsRemaining])
+
+  if (loading || isPosterExpired) return <Loading />
+  if (error) return <p>Error :(</p>
 
   return (
     <IonPage
       id="poster"
-      onClick={navigateNext}
+      onClick={handleClick}
       className={`remaining-${secondsRemaining}`}
     >
       <IonContent>
-        <IonText>
-          <h1>Guess the movie title</h1>
-        </IonText>
-        <div id="poster-container">
-          <div id="timer">{secondsRemaining}</div>
-          <IonImg src="assets/poster_witcher.png" />
+        <div id="timer">{secondsRemaining}</div>
+
+        <IonCard style={{ margin: 4 }}>
+          <IonCardHeader style={{ padding: 8 }}>
+            <IonCardTitle>Guess The Movie</IonCardTitle>
+            <IonText>tap screen to submit answer</IonText>
+          </IonCardHeader>
+        </IonCard>
+
+        <div style={{ maxWidth: 500 }}>
+          <IonCard style={{ margin: 4 }}>
+            <BlurAnimated>
+              <IonImg src={data && data.movie.posterPath} />
+            </BlurAnimated>
+          </IonCard>
         </div>
       </IonContent>
-      <IonFooter>
-        <div id="points">
-          <IonIcon icon={flash} />
-          12&apos;489
+
+      <div id="stats">
+        <div className="paper">
+          <div id="points">
+            <IonIcon icon={flash} />
+            12&apos;489
+          </div>
         </div>
-        <div id="lives">
-          <IonIcon icon={heartOutline} />
-          <IonIcon icon={heart} />
-          <IonIcon icon={heart} />
+        <div className="paper">
+          <div id="lives">
+            <IonIcon icon={heartOutline} />
+            <IonIcon icon={heart} />
+            <IonIcon icon={heart} />
+          </div>
         </div>
-      </IonFooter>
+      </div>
     </IonPage>
   )
 }

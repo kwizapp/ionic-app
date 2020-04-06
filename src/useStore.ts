d7cc0677bdc9bc388d54b102f5ed62a778fa488f
Nik Zaugg <zauggnik@gmail.com>
2020-04-06 09:17:15 +0200

    add types to store

--- src/useStore.ts
+++ src/useStore.ts
@@ -1,18 +1,38 @@
 import { Plugins } from '@capacitor/core'
-import create, { State } from 'zustand'
 import produce from 'immer'
+import create, { GetState, SetState, StoreApi } from 'zustand'
+
 import { QUESTION_TIME } from './settings'
 
 const { Storage } = Plugins
 
+interface Store {
+  alive: boolean
+  lives: number
+  points: number
+  timeRemaining: number
+  setState: (state: Partial<Store>) => void
+  resetState: () => void
+  setTimeRemaining: (seconds: number) => void
+  addPoints: () => void
+  removeLife: () => void
+}
+
 // setup immer to ensure data is changed immutably
 // this will allows us to "mutate" state without actually changing existing values
 // as immer always produces new objects when we mutate old ones
-const immer = (config: any) => (set: Function, get: Function, api: any) =>
-  config((fn: any) => set(produce(fn)), get, api)
+const immer = (config: any) => (
+  set: SetState<Store>,
+  get: GetState<Store>,
+  api: StoreApi<Store>,
+) => config((fn: any) => set(produce(fn)), get, api)
 
 // setup a persistence enhancer
-const persist = (config: any) => (set: Function, get: Function, api: any) =>
+const persist = (config: any) => (
+  set: SetState<Store>,
+  get: GetState<Store>,
+  api: StoreApi<Store>,
+) =>
   config(
     (args: any) => {
       set(args)
@@ -29,29 +49,35 @@ const initialState = {
   points: 0,
   timeRemaining: QUESTION_TIME,
 }
-const [useStore] = create(
+
+const [useStore] = create<Store>(
   persist(
     immer((set: any) => ({
       ...initialState,
-      setState(newState: any) {
+
+      setState(newState: Partial<Store>) {
         set(() => newState)
       },
+
       resetState() {
         set(() => initialState)
       },
-      setTimeRemaining(seconds: any) {
-        set((state: State) => {
+
+      setTimeRemaining(seconds: number) {
+        set((state: Store) => {
           state.timeRemaining = seconds
         })
       },
+
       addPoints() {
-        set((state: State) => {
+        set((state: Store) => {
           // TODO: use the points provided by the API instead
           state.points += (state.timeRemaining / QUESTION_TIME) * 1000
         })
       },
+
       removeLife() {
-        set((state: State) => {
+        set((state: Store) => {
           if (state.lives > 1) {
             state.lives -= 1
           } else {

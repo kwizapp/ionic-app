aad1ba7c8aacbb5707f10c628cca5d59ee93bd9e
Roland Schlaefli <rolandschlaefli@gmail.com>
2020-04-22 09:08:26 +0200

    Fix: Increase padding if the card is displayed without a poster

--- src/components/card/MovieCard.tsx
+++ src/components/card/MovieCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import classNames from 'classnames'
 
 interface Props {
   title: string
@@ -13,7 +14,10 @@ const MovieCard = ({ title, posterPath, onClick }: Props) => {
       className="flex m-2 border border-gray-100 rounded-md shadow-lg"
     >
       {posterPath && <img className="w-20 rounded-md" src={posterPath} />}
-      <div className="flex items-center p-4">
+
+      <div
+        className={classNames('flex items-center', posterPath ? 'p-4' : 'p-8')}
+      >
         <h2 className="font-sans font-medium tracking-wide text-md">{title}</h2>
       </div>
     </div>

ad5ada653c0e5c99234f2d76e40732daf5a0a0f2
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2020-04-27 09:26:02 +0200

    Use IonPage and IonContent because there is no StatsLayout

--- src/pages/Trivia.tsx
+++ src/pages/Trivia.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from '@apollo/client'
-import { IonButton, IonImg } from '@ionic/react'
+import { IonButton, IonContent, IonImg, IonPage } from '@ionic/react'
 import React from 'react'
 import { useHistory } from 'react-router'
 
@@ -21,44 +21,48 @@ const Trivia = () => {
   if (loading || !data) return <Loading />
 
   return (
-    <div className="m-3 flex flex-col items-center">
-      <div className="w-full relative">
-        <IonImg src={data?.movie?.posterPath} className="w-full h-full" />
+    <IonPage>
+      <IonContent>
+        <div className="m-3 flex flex-col items-center">
+          <div className="w-full relative">
+            <IonImg src={data?.movie?.posterPath} className="w-full h-full" />
 
-        <TriviaOverlay
-          className="items-center justify-center font-bold text-black text-2xl"
-          margin={overlayMargin}
-          style={{
-            top: overlayMargin,
-            height: '50px',
-          }}
-        >
-          <span>Did you know that?</span>
-        </TriviaOverlay>
+            <TriviaOverlay
+              className="items-center justify-center font-bold text-black text-2xl"
+              margin={overlayMargin}
+              style={{
+                top: overlayMargin,
+                height: '50px',
+              }}
+            >
+              <span>Did you know that?</span>
+            </TriviaOverlay>
 
-        <TriviaOverlay
-          className="flex-col text-lg"
-          margin={overlayMargin}
-          style={{
-            top: `calc(${overlayMargin} + 20%)`,
-            color: 'black',
-            fontWeight: 'bold',
-            padding: '10px',
-          }}
-        >
-          <div>
-            <p>Title: {data?.movie?.title}</p>
-            <p>Release Year: {data?.movie?.releaseYear}</p>
+            <TriviaOverlay
+              className="flex-col text-lg"
+              margin={overlayMargin}
+              style={{
+                top: `calc(${overlayMargin} + 20%)`,
+                color: 'black',
+                fontWeight: 'bold',
+                padding: '10px',
+              }}
+            >
+              <div>
+                <p>Title: {data?.movie?.title}</p>
+                <p>Release Year: {data?.movie?.releaseYear}</p>
+              </div>
+            </TriviaOverlay>
           </div>
-        </TriviaOverlay>
-      </div>
 
-      <div className="mt-5 w-11/12 text-center">
-        <IonButton className="w-full" onClick={navigateNext}>
-          Continue
-        </IonButton>
-      </div>
-    </div>
+          <div className="mt-5 w-11/12 text-center">
+            <IonButton className="w-full" onClick={navigateNext}>
+              Continue
+            </IonButton>
+          </div>
+        </div>
+      </IonContent>
+    </IonPage>
   )
 }
 

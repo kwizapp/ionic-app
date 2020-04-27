8186f9e2809bd826227aad9cc93e3e9f1e06ef7c
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2020-04-27 15:12:06 +0200

    Style trivia screen (#93)
    * Style trivia screen
    
    * Format
    
    * Create component for overlay
    
    * Get poster from question screen
    
    * Lint
    
    * Use IonPage and IonContent because there is no StatsLayout
    
    Co-authored-by: Nik Zaugg <zauggnik@gmail.com>

--- src/pages/Trivia.tsx
+++ src/pages/Trivia.tsx
@@ -1,20 +1,68 @@
-import { IonButton } from '@ionic/react'
+import { useQuery } from '@apollo/client'
+import { IonButton, IonContent, IonImg, IonPage } from '@ionic/react'
 import React from 'react'
 import { useHistory } from 'react-router'
 
-import StatsLayout from '../components/layouts/StatsLayout'
+import Loading from '../components/Loading'
+import TriviaOverlay from '../components/TriviaOverlay'
+import { MovieData, MOVIES } from './Question'
 
 const Trivia = () => {
   const history = useHistory()
 
+  const { loading, data } = useQuery<MovieData>(MOVIES, {
+    fetchPolicy: 'cache-only',
+  })
+
   const navigateNext = () => history.push('/poster')
 
-  return (
-    <StatsLayout>
-      <h1>Trivia Screen</h1>
+  const overlayMargin = '5%'
 
-      <IonButton onClick={navigateNext}>Next Question</IonButton>
-    </StatsLayout>
+  if (loading || !data) return <Loading />
+
+  return (
+    <IonPage>
+      <IonContent>
+        <div className="m-3 flex flex-col items-center">
+          <div className="w-full relative">
+            <IonImg src={data?.movie?.posterPath} className="w-full h-full" />
+
+            <TriviaOverlay
+              className="items-center justify-center font-bold text-black text-2xl"
+              margin={overlayMargin}
+              style={{
+                top: overlayMargin,
+                height: '50px',
+              }}
+            >
+              <span>Did you know that?</span>
+            </TriviaOverlay>
+
+            <TriviaOverlay
+              className="flex-col text-lg"
+              margin={overlayMargin}
+              style={{
+                top: `calc(${overlayMargin} + 20%)`,
+                color: 'black',
+                fontWeight: 'bold',
+                padding: '10px',
+              }}
+            >
+              <div>
+                <p>Title: {data?.movie?.title}</p>
+                <p>Release Year: {data?.movie?.releaseYear}</p>
+              </div>
+            </TriviaOverlay>
+          </div>
+
+          <div className="mt-5 w-11/12 text-center">
+            <IonButton className="w-full" onClick={navigateNext}>
+              Continue
+            </IonButton>
+          </div>
+        </div>
+      </IonContent>
+    </IonPage>
   )
 }
 

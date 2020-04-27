afcde6c191e6c4420d777d6b46a670001cdadfbd
Nik Zaugg <zauggnik@gmail.com>
2020-04-27 13:41:23 +0200

    add bestStreak to global store - as we want to report the best streak that the player had

--- src/useStore.ts
+++ src/useStore.ts
@@ -30,6 +30,7 @@ interface Store {
   timeRemaining: number
   movie: Movie
   streak: number
+  bestStreak: number
   guess: Guess
   setState: (state: Partial<Store>) => void
   resetState: () => void
@@ -76,6 +77,7 @@ const initialState: Partial<Store> = {
   pointDifference: 0, // the points gained or lost since the last question
   timeRemaining: QUESTION_TIME,
   streak: 0,
+  bestStreak: 0,
   movie: {
     imdbId: '',
     title: '',
@@ -148,6 +150,9 @@ const [useStore] = create<Store>(
       increaseStreak() {
         set((state: Store) => {
           state.streak++
+          if (state.streak > state.bestStreak) {
+            state.bestStreak = state.streak
+          }
         })
       },
 

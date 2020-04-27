1f50465c250ae5f425f7266a157f04f62770c3ab
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2020-04-27 11:47:12 +0200

    90 game over animation (#115)
    * Animate scored points
    
    * Animate emoji
    
    * Animate continue button
    
    * Animate text

--- src/pages/GameOver.tsx
+++ src/pages/GameOver.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import {
   IonPage,
   IonContent,
   IonButton,
+  useIonViewDidEnter,
   useIonViewWillLeave,
 } from '@ionic/react'
+import { animated, config, useChain, useSpring } from 'react-spring'
 import { useHistory } from 'react-router'
 import useStore from '../useStore'
 
@@ -14,9 +16,52 @@ const GameOver = () => {
   const history = useHistory()
   const { points, resetState } = useStore()
 
+  const [animReady, setAnimReady] = useState(false)
+
+  // setAnimReady: make sure the animations run everytime the page is loaded
+  useIonViewDidEnter(() => {
+    setAnimReady(true)
+  })
+
   useIonViewWillLeave(() => {
+    setAnimReady(false)
     resetState()
   })
+
+  // animate the emoji
+  const emojiAnimRef = useRef<any>()
+  const emojiAnimProps = useSpring({
+    ref: emojiAnimRef,
+    transform: !animReady ? 'scale(0)' : 'scale(1.1)',
+    opacity: !animReady ? 0 : 1,
+    config: config.wobbly,
+  })
+
+  // animate text
+  const textAnimRef = useRef<any>()
+  const textAnimProps = useSpring({
+    ref: textAnimRef,
+    opacity: !animReady ? 0 : 1,
+  })
+
+  // animate the number of scored points
+  const pointsAnimRef = useRef<any>()
+  const pointsAnimProps = useSpring({
+    ref: pointsAnimRef,
+    number: !animReady ? 0 : points,
+    opacity: !animReady ? 0 : 1,
+    config: config.default,
+  })
+
+  // animate continue button
+  const buttonAnimRef = useRef<any>()
+  const buttonAnimProps = useSpring({
+    ref: buttonAnimRef,
+    opacity: !animReady ? 0 : 1,
+    transform: !animReady ? 'scale(0)' : 'scale(1.0)',
+  })
+
+  useChain([emojiAnimRef, textAnimRef, pointsAnimRef, buttonAnimRef])
 
   const navigateNext = () => history.push('/')
 
@@ -24,20 +69,33 @@ const GameOver = () => {
     <IonPage>
       <IonContent className="text-center">
         <h1 className="text-2xl font-extrabold mt-20">Game Over</h1>
-        <p
-          style={{ fontFamily: 'Twitter Color Emoji' }}
-          className="text-10xl mt-3"
-        >
-          {emoji.get('disappointed_relieved')}
-        </p>
 
-        <p className="mt-5">You scored</p>
-        <p className="text-2xl font-bold">{points}</p>
-        <p>Points</p>
+        <animated.div style={emojiAnimProps}>
+          <p
+            style={{ fontFamily: 'Twitter Color Emoji' }}
+            className="mt-3 text-10xl"
+          >
+            {emoji.get('disappointed_relieved')}
+          </p>
+        </animated.div>
 
-        <IonButton className="mt-16" onClick={navigateNext}>
-          Start again
-        </IonButton>
+        <animated.div style={textAnimProps}>
+          <p className="mt-5">You scored</p>
+        </animated.div>
+
+        <animated.p style={pointsAnimProps} className="text-2xl font-bold">
+          {pointsAnimProps.number.interpolate((x: number) => x.toFixed(0))}
+        </animated.p>
+
+        <animated.div style={textAnimProps}>
+          <p>Points</p>
+        </animated.div>
+
+        <animated.div style={buttonAnimProps}>
+          <IonButton className="mt-16" onClick={navigateNext}>
+            Start again
+          </IonButton>
+        </animated.div>
       </IonContent>
     </IonPage>
   )

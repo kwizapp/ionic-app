4705f818241514d5ef2e1e05214d2656dbccfc29
Nik Zaugg <zauggnik@gmail.com>
2020-04-28 08:55:30 +0200

    122 extend trivia screen (#124)
    * Extend Trivia Screen with revenue, budget and popularity information
    
    * Add animated trivia screen displaying revenue, budget and popularity
    
    * cleanup
    
    * remove console.log

--- src/components/TriviaOverlay.tsx
+++ src/components/TriviaOverlay.tsx
@@ -12,7 +12,7 @@ const TriviaOverlay = ({ children, className, margin, style }: Props) => {
     <div
       className={`absolute flex rounded-md ${className}`}
       style={{
-        background: 'rgba(196, 196, 196, 0.75)',
+        background: 'rgba(255, 255, 255, 0.85)',
         border: '1px solid #B7B7B7',
         boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
         width: `calc(100% - 2 * ${margin})`,
